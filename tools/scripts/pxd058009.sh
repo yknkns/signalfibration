ef6a4408d92c417ca9da7a8172bb4cb19aad30d4
yknkns <naka0257yuu0430@keio.jp>
2026-06-24 14:20:03 +0900

    ⚡️ fix dir name

--- tools/scripts/pxd058009.sh
+++ tools/scripts/pxd058009.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-NAME="PXD058009"
+NAME="pxd058009"
 OUTDIR="./data/$NAME"
 URL="https://static-content.springer.com/esm/art%3A10.1038%2Fs44320-025-00141-1/MediaObjects/44320_2025_141_MOESM3_ESM.xlsx"
 
