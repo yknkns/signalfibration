58a55cc46c2e703cb30d28ea3d225d2c69525642
yknkns <naka0257yuu0430@keio.jp>
2026-06-26 19:44:49 +0900

    🚧 modify cptac-coad data sources and add data import

--- tools/scripts/cptac-coad.sh
+++ tools/scripts/cptac-coad.sh
@@ -4,8 +4,8 @@ set -eu
 NAME="cptac-coad"
 OUTDIR="./data/$NAME"
 
-PROTEO="https://linkedomics.org/cptac-colon/Human__CPTAC_COAD__PNNL__Proteome__TMT__03_01_2017__BCM__Gene__Tumor_Normal_log2FC.cct"
-PHOSPHO="https://linkedomics.org/cptac-colon/Human__CPTAC_COAD__PNNL__Phosphoproteome__TMT__03_01_2017__BCM__Site__Tumor_Normal_log2FC.cct.gz"
+PROTEO="https://linkedomics.org/cptac-colon/Human__CPTAC_COAD__PNNL__Proteome__TMT__03_01_2017__BCM__Gene__PNNL_Tumor_TMT_UnsharedLogRatio.cct"
+PHOSPHO="https://linkedomics.org/cptac-colon/Human__CPTAC_COAD__PNNL__Phosphoproteome__TMT__03_01_2017__BCM__Site__Tumor_PNNL_TMT_LogRatio.cct.gz"
 CLINICAL="https://linkedomics.org/cptac-colon/Human__CPTAC_COAD__MS__Clinical__Clinical__03_01_2017__CPTAC__Clinical__BCM.tsi"
 
 mkdir -p $OUTDIR
